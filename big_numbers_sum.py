bfbc6fec1883c24e649593b47ef4ec81b52ebc13
yasu <568200065@qq.com>
2019-11-24 13:49:44 +0900

    20191124_2

--- big_numbers_sum.py
+++ big_numbers_sum.py
@@ -1,10 +1,18 @@
+
+import datetime
+
 #计算1-1000000的和
 big_numbers=[value for value in range(1,1000001)]#创建1-1000000的列表
 sum=0
 print(min(big_numbers))#确保最小数为1 
 print(max(big_numbers))#确保最大数为1000000
 
+
+start = datetime.datetime.now()
 for number in big_numbers:
     sum=sum+number
 
 print(sum)
+
+end = datetime.datetime.now()
+print (end-start)
