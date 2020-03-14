b4ab031ae60fba7822d1293989d97a8e3b27c9e0
yasu <568200065@qq.com>
2020-03-14 15:47:47 +0900

    模拟登录 更新

--- HousePrice.py
+++ HousePrice.py
@@ -12,7 +12,7 @@ def getprice():
     ret = res.content.decode('gbk')  # ret为当前网页源代码
     # print(ret)
     result = etree.HTML(ret)  # 要使用XPath提取数据的话，必须先转换成XML数据
-    content_list=result.xpath('//div[@id="newhouse_loupai_list"]/ul/li')
+    content_list = result.xpath('//div[@id="newhouse_loupai_list"]/ul/li')
     for content in content_list:
         title = content.xpath('.//div[@class="nlcd_name"]/a/text()')
         title = title[0].strip() if len(title) > 0 else ''
@@ -22,5 +22,5 @@ def getprice():
 #TODO 完成代码重构
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     getprice()
---
+++ weibo_login.py
@@ -0,0 +1,9 @@
+# Name:taobao_login
+# Author:Yasu
+# Time:2020/3/14
+
+class Login(object):
+
+
+#TODO   代码待完善
+
