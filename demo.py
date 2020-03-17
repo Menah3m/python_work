8bb992b469cf1c2a835b564a4e0e099fa7479c90
yasu <568200065@qq.com>
2020-03-18 00:22:05 +0900

    20200318

--- demo.py
+++ demo.py
@@ -1,6 +1,20 @@
-from access_limited import Student
+# from access_limited import Student
+#
+# stu = Student('Bob', '20', '18')
+# stu.shuchu()
 
-stu = Student('Bob', '20', '18')
-stu.shuchu()
+movie = '大侦探'
+count = 5
+ticket = 14.9
+total = count * ticket
 
 
+
+message = '''
+电影；{0}
+人数：{2}
+单价：{1}
+总价：{3}
+'''.format(movie, count, ticket, total)
+
+print(message)
