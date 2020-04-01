6648de2d4086a1243b9048fa9dd24a10211a65f8
yasu <568200065@qq.com>
2020-04-01 22:31:08 +0900

    20200401 commit

--- demo.py
+++ demo.py
@@ -3,18 +3,7 @@
 # stu = Student('Bob', '20', '18')
 # stu.shuchu()
 
-movie = '大侦探'
-count = 5
-ticket = 14.9
-total = count * ticket
+str = 'we are fff'
+new_str = ''
 
-
-
-message = '''
-电影；{0}
-人数：{2}
-单价：{1}
-总价：{3}
-'''.format(movie, count, ticket, total)
-
-print(message)
+print(str.join('%20'))
---
+++ send_mail.py
@@ -0,0 +1,13 @@
+# Name:send_mail
+# Author:Yasu
+# Time:2020/3/17
+
+import yagmail
+
+USER = 'dawnwhite0@163.com'
+PASSWORD = ' '
+HOST = 'smtp.163.com'
+CONTENT =
+
+if __name__ = '__main__':
+    yag = yagmail.SMTP(USER, PASSWORD, HOST)
